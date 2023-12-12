21a77bf6f1d860ca3b21b55696d1ae14588f2c7c
809949 <gunasrivinjam@gmail.com>
2023-12-12 10:37:02 +0530

    snd commit

--- app/admin.py
+++ app/admin.py
@@ -4,4 +4,4 @@ from django.contrib import admin
 from app.models import*
 admin.site.register(Topic)
 admin.site.register(webpage)
-
+admin.site.register(Wife)
--- app/models.py
+++ app/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
 
 # Create your models here.
 class Topic(models.Model):
     Topic_name=models.CharField(max_length=100,primary_key=True)
-    def __str__(self) -> str:
-        return self.Topic_name
+    def __str__(self) :
+        return str(self.pk)
 
 
 
@@ -15,7 +14,12 @@ class webpage(models.Model):
     Topic_name=models.ForeignKey(Topic,on_delete=models.CASCADE)
     Name=models.CharField(max_length=100)
     Url=models.URLField()
-    def __str__(self) -> str:
+    def __str__(self) :
         return self.Name
-
+class Wife(models.Model):
+    Name=models.ForeignKey(webpage,on_delete=models.CASCADE)
+    w_name=models.CharField(max_length=100)
+    def __str__(self) :
+        return self.w_name
+   
 
